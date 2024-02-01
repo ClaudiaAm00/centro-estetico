6d5acf7a86812cbde2b7e3ad8fc3f2c9f05ac678
silvialaserra <silvialaserra1998@gmail.com>
2024-02-01 15:13:19 +0100

    aggiunta query listaNuovePrestazioni

--- src/main/java/it/corso/dao/PrestazioneDao.java
+++ src/main/java/it/corso/dao/PrestazioneDao.java
@@ -1,8 +1,16 @@
 package it.corso.dao;
+import java.util.List;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import it.corso.model.Prestazione;
+
 
 public interface PrestazioneDao extends CrudRepository<Prestazione, Integer>
 {
 
+	// query per le nuove prestazioni offerte, limitata agli ultimi 5 record
+	@Query(value = "SELECT * FROM prestazione ORDER BY id=:p DESC LIMIT 5",	nativeQuery = true)
+	List<Prestazione> listaNuovePrestazioni(@Param("p") int id);
+	
 }
