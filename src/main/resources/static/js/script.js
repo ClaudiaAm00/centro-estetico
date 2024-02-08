ba45fe818635ac8415405857095604a74a0f0f3f
Claudia Amoruso <claudiaamoruso1@gmail.com>
2024-02-08 11:28:16 +0100

    update js + css

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,21 +1,29 @@
 /**
  * 
  */
- function aggiungiOmbra(){
-
-    var card = document.getElementById("card"); 
-
-    card.addEventListener('mouseover', function(){
-        card.style.boxShadow = 'inset 0 0 50px 40px rgb(40, 40, 40)';
-    });
-    
-
-    card.addEventListener('mouseout', function(){
-        card.style.boxShadow = 'none';
+ function aggiungiOmbra() {
+    document.addEventListener('DOMContentLoaded', function() {
+        // Seleziona tutte le card all'interno del div specifico
+        var cards = document.querySelectorAll('[id^="card"]');
+        
+        cards.forEach(function(card) {
+            // Aggiungi un listener per l'evento mouseover a ciascuna card
+            card.addEventListener('mouseover', function() {
+                // Applica l'ombra quando il mouse passa sopra alla card
+                card.style.boxShadow = 'inset 0 0 50px 40px rgb(40, 40, 40)';
+            });
+            
+            // Aggiungi un listener per l'evento mouseout a ciascuna card
+            card.addEventListener('mouseout', function() {
+                // Rimuovi l'ombra quando il mouse esce dalla card
+                card.style.boxShadow = 'none';
+            });
+        });
     });
 }
-
+// Chiama la funzione per aggiungere l'ombra alle card
 aggiungiOmbra();
+
 
 /*CONTROLLO DIMENSIONE FILE IMMAGINE*/ 
 document.getElementById('fileInput').addEventListener('change', function() {
