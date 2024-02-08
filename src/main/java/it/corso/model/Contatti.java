6b7c2b2a858196e77f93c4639ce721f83845627d
Mariachiara1 <mariachiara.allori@gmail.com>
2024-02-08 10:07:58 +0100

    Model e Dao di Contatti e creazione Service

---
+++ src/main/java/it/corso/model/Contatti.java
@@ -0,0 +1,83 @@
+package it.corso.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "contatti")
+public class Contatti 
+{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column
+	private String nome;
+	
+	@Column
+	private String cognome;
+	
+	@Column
+	private String email;
+	
+	@Column
+	private String telefono;
+	
+	@Column
+	private String testo;
+	
+	// incapsulamento
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCognome() {
+		return cognome;
+	}
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getTesto() {
+		return testo;
+	}
+
+	public void setTesto(String testo) {
+		this.testo = testo;
+	}
+	
+}
