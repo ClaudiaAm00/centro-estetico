611958dd4b4fb6531ad280168a97b4d495ad03cd
silvialaserra <silvialaserra1998@gmail.com>
2024-02-08 15:35:59 +0100

    collegamento database - tabella contatti in area riservata

--- src/main/java/it/corso/controller/AreaRiservataController.java
+++ src/main/java/it/corso/controller/AreaRiservataController.java
@@ -1,9 +1,17 @@
 package it.corso.controller;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import it.corso.model.Amministrazione;
+import it.corso.model.Contatti;
+import it.corso.model.Staff;
+import it.corso.service.ContattiService;
 import jakarta.servlet.http.HttpSession;
 
 // localhost8080:/areariservata
@@ -11,6 +19,9 @@ import jakarta.servlet.http.HttpSession;
 @RequestMapping("/areariservata")
 public class AreaRiservataController 
 {
+	@Autowired
+	private ContattiService contattiService;
+	
 	@GetMapping
 	public String getPage(
 			HttpSession session,
@@ -21,6 +32,16 @@ public class AreaRiservataController
 		
 		Amministrazione amministrazione = (Amministrazione) session.getAttribute("amministratore");
 		model.addAttribute("amministratore", amministrazione);
+		
+		List<Contatti> contatti = contattiService.getAllContatti();
+		model.addAttribute("contatti", contatti);
+		
 		return "/areariservata";	
+	}
+	
+	@GetMapping("/cancellamessaggio")
+	public String gestioneCancellazione(@RequestParam("id") int id){
+		contattiService.cancellaContatti(contattiService.getContattiById(id));
+		return "redirect:/areariservata";
 	}
 }
