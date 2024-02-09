7d7f1720a3cb94a39cf7fcdeb7d643d579e30f9e
Claudia Amoruso <claudiaamoruso1@gmail.com>
2024-02-09 12:34:32 +0100

    update js + css

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,7 +1,7 @@
 /**
  * 
  */
- /*function aggiungiOmbra() {
+ function aggiungiOmbra() {
     document.addEventListener('DOMContentLoaded', function() {
         // Seleziona tutte le card all'interno del div specifico
         var cards = document.querySelectorAll('[id^="card"]');
@@ -10,33 +10,40 @@
             // Aggiungi un listener per l'evento mouseover a ciascuna card
             card.addEventListener('mouseover', function() {
                 // Applica l'ombra quando il mouse passa sopra alla card
-                // card.style.boxShadow = 'inset 0 0 50px 40px rgb(40, 40, 40)';
-                card.style.filter = 'brightness(70%)';
+                card.style.boxShadow = 'inset 0 0 50px 40px rgb(40, 40, 40)';
+                //card.style.filter = 'brightness(70%)';
             });
             
             // Aggiungi un listener per l'evento mouseout a ciascuna card
             card.addEventListener('mouseout', function() {
                 // Rimuovi l'ombra quando il mouse esce dalla card
-                // card.style.boxShadow = 'none';
-                card.style.filter = 'brightness(100%)';
+                card.style.boxShadow = 'none';
+                //card.style.filter = 'brightness(100%)';
             });
         });
     });
 }
-// Chiama la funzione per aggiungere l'ombra alle card
-aggiungiOmbra();*/
 
-function aggiungiOmbra() {
+aggiungiOmbra();
+
+/*function aggiungiOmbra() {
     document.addEventListener('DOMContentLoaded', function() {
-        var cards = document.querySelectorAll('.card-with-overlay');
+        let cards = document.querySelectorAll('.card-with-overlay');
+        let staff = document.getElementsByClassName(infoStaff);
+        let img = document.getElementsByClassName(imgStaff);
         
         cards.forEach(function(card) {
-            var overlay = document.createElement('div'); // Creare l'elemento overlay
+            let overlay = document.createElement('div'); // Creare l'elemento overlay
             overlay.classList.add('overlay'); // Aggiungere la classe overlay
             card.appendChild(overlay); // Aggiungere overlay come figlio della card
             
             card.addEventListener('mouseover', function() {
+				staff.style.zIndex = '5';
+				img.style.zIndex = '0';
                 overlay.style.opacity = '0.5'; // Opacità dimezzata
+                
+               
+                
             });
             
             card.addEventListener('mouseout', function() {
@@ -48,7 +55,7 @@ function aggiungiOmbra() {
 
 // Chiama la funzione per aggiungere l'overlay alle card
 aggiungiOmbra();
-
+*/
 
 
 /*CONTROLLO DIMENSIONE FILE IMMAGINE*/ 
